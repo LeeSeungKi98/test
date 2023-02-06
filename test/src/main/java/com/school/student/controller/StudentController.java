db77629477725f394e7e183ae52160499eea65cb
lsk <tmdrlk365w@naver.com>
2023-02-06 15:27:03 +0900

    controller

---
+++ test/src/main/java/com/school/student/controller/StudentController.java
@@ -0,0 +1,11 @@
+package com.school.student.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class StudentController {
+
+	@GetMapping("/")
+	public void index() {
+		
+	}
+}
